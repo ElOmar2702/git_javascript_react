d29ca270cf058c3c063115f67870adeacedca413
Omar Mayorga <omayorgacruz1@gmail.com>
2026-08-24 10:49:47 -0500

    Reto 1: funciones, parámetros y return

--- tienda-react/practica-js.js
+++ tienda-react/practica-js.js
@@ -1,6 +1,45 @@
-function calcularTotal(precio, cantidad) {
- const total = precio * cantidad;
+function Suma(a, b) {
+ const total = a + b;
  return total;
 }
-const resultado = calcularTotal(50000, 3);
-console.log(resultado);
+const resultado = Suma(5, 3);
+
+function Resta(a, b)
+{
+ const total = a - b
+ return total;
+}
+const resultado2 = Resta(8, 3);
+
+function multiplicar(a, b)
+{
+ const total = a * b
+ return total;
+}
+const resultado3 = multiplicar(5, 2);
+
+function dividir(a, b)
+{
+ const total = (a / b)
+ return total;
+}
+const resultado4 = dividir(16, 4);
+
+function Promedio(nota1, nota2, nota3)
+{
+ const total = (nota1 + nota2 + nota3) / 3
+ return total;
+}
+const resultado5 = Promedio(16, 4, 8);
+
+
+
+
+console.log("Resultado Suma:",resultado);
+console.log("Resultado Resta:",resultado2);
+console.log("Resultado Multiplicacion:",resultado3);
+console.log("Resultado Division:",resultado4);
+console.log("Resultado Promedio:",resultado5);
+
+// El "console.log" es para escribir o mostrar un mensaje en pantalla, el "return" sirve para devolver o retornar
+// un resultado o un procedimiento en consola
