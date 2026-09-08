b7d753e499d8d7cf4f3b75f4bf92cacd319331a5
Omar Mayorga <omayorgacruz1@gmail.com>
2026-09-08 00:25:24 -0500

    MISION 2

--- tienda-react/src/components/ProductoCard.jsx
+++ tienda-react/src/components/ProductoCard.jsx
@@ -1,13 +1,22 @@
 function ProductoCard({ producto }) {
- const estado = producto.stock > 0 ? 'Disponible' : 'Agotado';
- return (
- <article className="producto-card">
- <h2>{producto.nombre}</h2>
- <p>Categoría: {producto.categoria}</p>
- <p>Precio: ${producto.precio}</p>
- <p>Stock: {producto.stock}</p>
- <strong>{estado}</strong>
- </article>
- );
+  const {
+    nombre,
+    precio,
+    categoria,
+    stock
+  } = producto;
+
+  const estado = producto.stock > 0 ? 'Disponible' : 'Agotado';
+
+  return (
+    <article className="producto-card">
+      <h2>{nombre}</h2>
+      <p>Categoría: {categoria}</p>
+      <p>Precio: ${precio}</p>
+      <p>Stock: {stock}</p>
+      <strong>{estado}</strong>
+    </article>
+  );
 }
+
 export default ProductoCard;
